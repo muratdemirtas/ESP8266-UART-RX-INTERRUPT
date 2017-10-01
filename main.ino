487a9ecc8ff03500c9992dc00ff2bb87bda0f7e3
Murat Demirtaş <muratdemirtastr@gmail.com>
2017-10-01 16:05:12 +0300

    Update main.ino

--- main.ino
+++ main.ino
@@ -5,14 +5,12 @@
 #include "WProgram.h"
 #endif
 
-//Extract Espressif methods
-extern "C" {
-#include "user_interface.h"
-#include "espconn.h" 
-}
+#include "commonHeaders.h"
 
 #include <stdarg.h>	
 
+
+//Define debug types for serial debugging.
 enum debugTypes {
 	ERROR = 0x0001,
 	BOOT = 0x0002,
@@ -27,8 +25,20 @@ enum debugTypes {
 	SCAN = 0x0800
 };
 
+typedef enum
+{
+	UART_ERROR_OCCURRED,
+	UART_NOERROR_OCCURRED,
+	UART_RECEIVING_DATA,
+	UART_RECEIVED_DATA,
+	UART_RECEIVE_TIMEOUT
+}uart_events_t;
+
+uart_events_t UART_EVENT_FLAG = UART_NOERROR_OCCURRED;
+
 //Print debug messages using serial monitor.
 void ICACHE_FLASH_ATTR printMsg(debugTypes types, bool newline, const char* format ...) {
+	
 
 		char message[300];
 		///Macro for arguments
@@ -43,41 +53,121 @@ void ICACHE_FLASH_ATTR printMsg(debugTypes types, bool newline, const char* form
 		va_end(args);
 
 }
+#define UART_RX_BUFFER_SIZE 1024
+uint8_t UART_RX_BUFFER[UART_RX_BUFFER_SIZE];
+long int UART_RX_RECEIVE_COUNT = 0;
 
-#define MY_TASK_PRIORITY 0
-#define MY_QUEUE_SIZE    1
 
-static os_event_t my_queue[MY_QUEUE_SIZE];
+#define UART_RX_TASK_PRIORITY 1
+#define UART_TASK_PRIORITY 0
+
+#define UART_RX_QUEUE_SIZE 6
+#define UART_TASK_QUEUE_SIZE 6
+#define UART_RX_ESCAPE_CHARACTER '\r'
+
+static os_timer_t UART_RX_TASK_Timer;
+static os_timer_t UART_RX_TimeoutTimer;
+static os_timer_t UART_TASK_Timer;
+
+
+static os_event_t UART_RX_QUEUE[UART_RX_QUEUE_SIZE];
+static os_event_t UART_TASK_QUEUE[UART_TASK_QUEUE_SIZE];
+
+long int UART_RX_CYCLE_IN_MS = 5;
+long int UART_TASK_CYCLE_IN_MS = 2;
+long int UART_RX_TIMEOUT_CYCLE_IN_MS = 500;
+
 volatile uint32_t value = 0;
-static os_timer_t task_start_timer;
+
 
 //os_timer timeout callback
-void run_task(void*) {
-	system_os_post(MY_TASK_PRIORITY, 0, 0);
+void Temp_Run_UART_RX_Task(void*) {
+	system_os_post(UART_RX_TASK_PRIORITY, 0, 0);
 }
 
-//thread entry point,no for while allowed(!)
-void my_task(os_event_t *) {
-	
+//os_timer timeout callback
+void Temp_Run_UART_Task(void*) {
+	system_os_post(UART_TASK_PRIORITY, 0, 0);
+}
 
+void Temp_Run_UART_Timeout_Task(void*)
+{
+	Serial.println("UART RX Timeout,clear buffer");
+	UART_RX_RECEIVE_COUNT = 0;
+	os_memset(UART_RX_BUFFER, 0, UART_RX_BUFFER_SIZE);
+	UART_EVENT_FLAG = UART_RECEIVE_TIMEOUT;
+
+}
+
+//thread entry point
+void UART_RX_TASK_Handler(os_event_t *) {
+	
 	if (Serial.available() > 0)
 	{
-		char k = Serial.read();
-		printMsg(BOOT, true, "Received: %c", k);
+		char Temp_Char = Serial.read();
+
+		if (Temp_Char == UART_RX_ESCAPE_CHARACTER)
+		{
+			UART_EVENT_FLAG = UART_RECEIVED_DATA;
+			UART_RX_RECEIVE_COUNT = 0;
+			ets_timer_disarm(&UART_RX_TimeoutTimer);
+		}
+
+		else
+		{
+			UART_EVENT_FLAG = UART_RECEIVING_DATA;
+			UART_RX_BUFFER[UART_RX_RECEIVE_COUNT] = Temp_Char;
+			UART_RX_RECEIVE_COUNT++;
+			os_timer_arm(&UART_RX_TimeoutTimer, UART_RX_TIMEOUT_CYCLE_IN_MS, 0);
+		}
+		
 	}
-	// start the task again in 50  msec
-	os_timer_arm(&task_start_timer, 50 /*ms*/, 0 /*once*/);
+
+	os_timer_arm(&UART_RX_TASK_Timer, UART_RX_CYCLE_IN_MS /*ms*/, 0 /*once*/);
+}
+
+void UART_TASK_Handler(os_event_t *) 
+{
+	
+
+	if (UART_EVENT_FLAG == UART_RECEIVED_DATA)
+	{
+		UART_EVENT_FLAG = UART_NOERROR_OCCURRED;
+		printMsg(OS, true, "alinan %s", (char*)UART_RX_BUFFER);
+		os_memset(UART_RX_BUFFER, 0, UART_RX_BUFFER_SIZE);
+	}
+		
+
+	os_timer_arm(&UART_TASK_Timer, UART_TASK_CYCLE_IN_MS /*ms*/, 0 /*once*/);
 }
 
 void setup() {
 
 	Serial.begin(9600);
-	system_os_task(my_task,
-		MY_TASK_PRIORITY, my_queue,
-		MY_QUEUE_SIZE);
 
-	os_timer_setfn(&task_start_timer, (os_timer_func_t*)&run_task, 0);
-	run_task(0);
+	//thread olusturma
+	system_os_task(UART_RX_TASK_Handler,
+		UART_RX_TASK_PRIORITY, UART_RX_QUEUE,
+		UART_RX_QUEUE_SIZE);
+
+	system_os_task(UART_TASK_Handler,
+		UART_TASK_PRIORITY, UART_TASK_QUEUE,
+		UART_TASK_QUEUE_SIZE);
+
+	//timer olusturma
+	os_timer_setfn(&UART_RX_TASK_Timer, 
+			(os_timer_func_t*)&Temp_Run_UART_RX_Task, 0);
+
+	os_timer_setfn(&UART_TASK_Timer,
+		(os_timer_func_t*)&Temp_Run_UART_Task, 0);
+
+	os_timer_setfn(&UART_RX_TimeoutTimer,
+		(os_timer_func_t*)&Temp_Run_UART_Timeout_Task, 0);
+
+	//taski baslatma
+	Temp_Run_UART_RX_Task(0);
+	Temp_Run_UART_Task(0);
+
 }
 
 // the loop function runs over and over again until power down or reset
